b2d47336293815fdc662b898e6e227137c3424e4
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-21 10:33:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,67 +6,52 @@ const headsBet = document.getElementById("heads-bet");
 const tailsBet = document.getElementById("tails-bet");
 const betAmount = document.getElementById("bet-amount");
 let currentBetAmount = 0;
+const connectButton = document.getElementById("connect-button");
+let publicKey;
 
-flipButton.addEventListener("click", () => {
-  const flip = Math.round(Math.random());
-  if (flip === 0) {
-    result.innerHTML = "Heads";
-  } else {
-    result.innerHTML = "Tails";
-  }
+(async () => {
+  const solana = await Solana.new('https://testnet.solana.com');
+  const phantom = await Phantom.new('https://testnet.phantom.us');
 
-  if (headsBet.checked && result.innerHTML === "Heads") {
-    resultMessage.innerHTML = "You win!";
-    currentBetAmount = betAmount.value * 2;
-    betAmountResult.innerHTML = `You won ${currentBetAmount}`;
-  } else if (tailsBet.checked && result.innerHTML === "Tails") {
-    resultMessage.innerHTML = "You win!";
-    currentBetAmount = betAmount.value * 2;
-    betAmountResult.innerHTML = `You won ${currentBetAmount}`;
-  } else {
-    resultMessage.innerHTML = "You lose.";
-    currentBetAmount = 0;
-    betAmountResult.innerHTML = `You lost ${betAmount.value}`;
-  }
-  const solana = await Solana.new('https://testnet.solana.com');const recipientPublicKey = '<recipient-public-key>';
-const senderPrivateKey = '<sender-private-key>';
-const amount = 1000000;
+  flipButton.addEventListener("click", async () => {
+    const flip = Math.round(Math.random());
+    if (flip === 0) {
+      result.innerHTML = "Heads";
+    } else {
+      result.innerHTML = "Tails";
+    }
 
-const result = await solana.sendTransaction(recipientPublicKey, senderPrivateKey, amount);
-  const publicKey = '<public-key>';
-const balance = await solana.getBalance(publicKey);
-console.log(balance);
-  const transactionId = '<transaction-id>';
-const transaction = await solana.getTransaction(transactionId);
-console.log(transaction);
- const phantom = await Phantom.new('https://testnet.phantom.us');const options = {
-    appName: 'YourAppName',
-    appLogoUrl: 'https://your-app-logo-url.com',
-    connectionType: 'permissionless',
-};
-const connection = await phantom.connect(options);
-  const isConnected = await phantom.isConnected();
-console.log(isConnected);
-  const publicKey = await phantom.getPublicKey();
-console.log(publicKey);
-  const transaction = {
-    amount: 1000000,
-    recipient: '<recipient-public-key>',
-};
-const signature = await phantom.signTransaction(transaction);
-console.log(signature);
-  const connectButton = document.getElementById("connect-button");
+    if (headsBet.checked && result.innerHTML === "Heads") {
+      resultMessage.innerHTML = "You win!";
+      currentBetAmount = betAmount.value * 2;
+      betAmountResult.innerHTML = `You won ${currentBetAmount}`;
+      const transaction = {
+        amount: currentBetAmount,
+        recipient: publicKey,
+      };
+      const signature = await phantom.signTransaction(transaction);
+      await solana.sendTransaction(transaction, signature);
+    } else if (tailsBet.checked && result.innerHTML === "Tails") {
+      resultMessage.innerHTML = "You win!";
+      currentBetAmount = betAmount.value * 2;
+      betAmountResult.innerHTML = `You won ${currentBetAmount}`;
+      const transaction = {
+        amount: currentBetAmount,
+        recipient: publicKey,
+      };
+      const signature = await phantom.signTransaction(transaction);
+      await solana.sendTransaction(transaction, signature);
+    } else {
+      resultMessage.innerHTML = "You lose.";
+      currentBetAmount = 0;
+      betAmountResult.innerHTML = `You lost ${betAmount.value}`;
+      const transaction = {
+        amount: betAmount.value,
+        recipient: '<recipient-public-key>',
+      };
+      const signature = await phantom.signTransaction(transaction);
+      await solana.sendTransaction(transaction, signature);
+    }
+  });
 
-connectButton.addEventListener("click", async () => {
-  const options = {
-      appName: 'YourAppName',
-      appLogoUrl: 'https://your-app-logo-url.com',
-      connectionType: 'permissionless',
-  };
-  const connection = await phantom.connect(options);
-  if (connection) {
-      console.log("User connected");
-  } else {
-      console.log("User not connected");
-  }
-});
+  connectButton.add
