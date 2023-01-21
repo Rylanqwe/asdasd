1a2b77a7f18804bd9542fa667e7d3ec3ba676a02
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-01-20 21:56:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const flipButton = document.getElementById("flip-button");
 const result = document.getElementById("result");
 const resultMessage = document.getElementById("result-message");
+const betAmountResult = document.getElementById("bet-amount-result");
 const headsBet = document.getElementById("heads-bet");
 const tailsBet = document.getElementById("tails-bet");
+const betAmount = document.getElementById("bet-amount");
+let currentBetAmount = 0;
 
 flipButton.addEventListener("click", () => {
   const flip = Math.round(Math.random());
@@ -14,9 +17,15 @@ flipButton.addEventListener("click", () => {
 
   if (headsBet.checked && result.innerHTML === "Heads") {
     resultMessage.innerHTML = "You win!";
+    currentBetAmount = betAmount.value * 2;
+    betAmountResult.innerHTML = `You won ${currentBetAmount}`;
   } else if (tailsBet.checked && result.innerHTML === "Tails") {
     resultMessage.innerHTML = "You win!";
+    currentBetAmount = betAmount.value * 2;
+    betAmountResult.innerHTML = `You won ${currentBetAmount}`;
   } else {
     resultMessage.innerHTML = "You lose.";
+    currentBetAmount = 0;
+    betAmountResult.innerHTML = `You lost ${betAmount.value}`;
   }
 });
